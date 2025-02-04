67d41a70dd1e75b2eda92cbfa707015da2e99db5
itsashishrath <anandashish654@gmail.com>
2025-02-05 00:24:08 +0530

    fixed keys leak

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-export GOOGLEAPIKEY="AIzaSyBxpDLEWcZIXBS1Gf2Si8JdOLTuvXrLVu8"
-export GEMINISTUDIOKEY2 = "AIzaSyCYtXLllulrHSAH_Sb90bsgBdSwEJp9kFE"
-
 poetry install
 
 python manage.py collectstatic --no-input
