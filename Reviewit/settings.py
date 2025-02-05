73cc6484592392f53b80948d0adffeffe0803777
itsashishrath <anandashish654@gmail.com>
2025-02-05 18:04:53 +0530

    Fix

--- Reviewit/settings.py
+++ Reviewit/settings.py
@@ -43,9 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'apiReview.apps.ApireviewConfig',
-    'shakespeare_translator'
-]
+    'apiReview.apps.ApireviewConfig'
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Reviewit/urls.py
+++ Reviewit/urls.py
@@ -21,6 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wel/', include("apiReview.urls")), 
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('shakespeare/', include('shakespeare_translator.urls')),  
-]
+    path('', TemplateView.as_view(template_name='index.html'))
+    ]
